04a440d5620e9582003ee468dbf2ee238929947f
obliv1on <ivan.peshekhonov@gmail.com>
2019-05-23 09:58:22 +0300

    Updates

---
+++ precache-manifest.bcf3e3dab5637d3feb7c51c5b369bc6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9725628124656b4583ba",
+    "url": "/booklet_vk/static/js/runtime~main.00bf6e5a.js"
+  },
+  {
+    "revision": "8c463fe7ea76c7f4da9b",
+    "url": "/booklet_vk/static/js/main.b26b4af0.chunk.js"
+  },
+  {
+    "revision": "2996c583335784ad24a3",
+    "url": "/booklet_vk/static/js/2.f8604d79.chunk.js"
+  },
+  {
+    "revision": "8c463fe7ea76c7f4da9b",
+    "url": "/booklet_vk/static/css/main.c9169ae2.chunk.css"
+  },
+  {
+    "revision": "2996c583335784ad24a3",
+    "url": "/booklet_vk/static/css/2.f952a7a9.chunk.css"
+  },
+  {
+    "revision": "ba13bedfbaa2f719d54176c7c91adec7",
+    "url": "/booklet_vk/index.html"
+  }
+];
