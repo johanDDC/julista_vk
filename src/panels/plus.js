afc8aa55546256c3a77968b5a6cbcc0a2285ec18
ivan <ivan.peshekhonov@gmail.com>
2019-07-19 19:15:34 +0300

    Design corrections (booklet plus)

--- src/panels/plus.js
+++ src/panels/plus.js
@@ -45,37 +45,25 @@ class Plus extends React.Component {
                     noShadow={true}
                     left={<Div style={{fontWeight: 'bold', fontSize: '20px'}}><span>Booklet Plus</span></Div>}>
                 </PanelHeader>
-                <FixedLayout>
+                <FixedLayout style={{width: "100%"}}>
                     <Div className="plusBlueBack">
                         <span>Расширь возможности своего дневника</span>
                         <PlusIcon selected={true} size="36" stroke_color={"#ffffff"}/>
                     </Div>
                 </FixedLayout>
-                <div className="plusOfferContainer" style={{marginTop: "70px"}}>
-                    <Div className="plusOfferInfo">
-                        <span className="plusOfferTitle">
-                            Уведомления
-                        </span>
-                        <span className="plusOfferText">
-                            — Новые оценки
-                        </span>
-                        <span className="plusOfferText">
-                            — Крепостное право
-                        </span>
-                        <span className="plusOfferText">
-                            — Острые ощущения
-                        </span>
-                        <span className="plusOfferText">
-                            — Пошёл в жопу
-                        </span>
-                        <div className="plusOfferAdditional">
-                            <span className="plusOfferText">
-                                149 рублей за год — как бордель на окраине Мухосранска
-                            </span>
-                        </div>
-                    </Div>
-                    <Button level="tertiary" className="plusButton">
-                        Купить — 149 руб.
+                <div style={{padding: "0 16px 0 16px", marginBottom: "8px"}}>
+                    <Button level="tertiary" className="plusVkContainer">
+                        Бот для ВКонтакте
+                    </Button>
+                </div>
+                <div style={{padding: "0 16px 0 16px", marginBottom: "8px"}}>
+                    <Button level="tertiary" className="plusTelegramContainer">
+                        Бот для Телеграм
+                    </Button>
+                </div>
+                <div style={{padding: "0 16px 0 16px", marginBottom: "8px"}}>
+                    <Button level="tertiary" className="plusAliceContainer">
+                        Навык для Алисы
                     </Button>
                 </div>
                 <div className="plusOfferContainer">
@@ -101,17 +89,34 @@ class Plus extends React.Component {
                     </Div>
                     <Button level="tertiary" className="plusButton"
                             style={{background: "linear-gradient(rgb(77 229 101), rgb(74 195 122))"}}>
-                        Пригласить содомитов
+                        Пригласить друзей
                     </Button>
                 </div>
-                <div style={{padding: "0 17px 0 17px", marginBottom: "10px"}}>
-                    <Button level="tertiary" className="plusAliceContainer">
-                        Навык для Алисы
-                    </Button>
-                </div>
-                <div style={{padding: "0 17px 0 17px", marginBottom: "10px"}}>
-                    <Button level="tertiary" className="plusVkContainer">
-                        Бот ВКонтакте
+                <div className="plusOfferContainer">
+                    <Div className="plusOfferInfo">
+                        <span className="plusOfferTitle">
+                            Уведомления
+                        </span>
+                        <span className="plusOfferText">
+                            — Новые оценки
+                        </span>
+                        <span className="plusOfferText">
+                            — Текущий/следующий урок
+                        </span>
+                        <span className="plusOfferText">
+                            — События в классе
+                        </span>
+                        <span className="plusOfferText">
+                            — Отчётность за день
+                        </span>
+                        <div className="plusOfferAdditional">
+                            <span className="plusOfferText">
+                                149 рублей за год — как шаверма в центре Москвы
+                            </span>
+                        </div>
+                    </Div>
+                    <Button level="tertiary" className="plusButton">
+                        Купить — 149 руб.
                     </Button>
                 </div>
             </Panel>
