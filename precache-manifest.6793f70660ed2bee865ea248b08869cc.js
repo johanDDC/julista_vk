6a0b8019228aa03d3b4b708c36a90dbad3e54a96
ivan <ivan.peshekhonov@gmail.com>
2019-10-21 11:02:00 +0300

    Updates

--- precache-manifest.6793f70660ed2bee865ea248b08869cc.js
+++ precache-manifest.6793f70660ed2bee865ea248b08869cc.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "51ed817fc98a60306be3a27a5b46aadc",
+    "revision": "30052fcb0c576a9c08aeaf02c5a62789",
     "url": "/julista_vk/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/julista_vk/static/css/2.a83908c5.chunk.css"
   },
   {
-    "revision": "8c8e0d2475716c80c218",
+    "revision": "f1335541fde03c9f60a1",
     "url": "/julista_vk/static/css/main.b6865357.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/julista_vk/static/js/2.83ed1919.chunk.js"
   },
   {
-    "revision": "8c8e0d2475716c80c218",
-    "url": "/julista_vk/static/js/main.b3a6f31d.chunk.js"
+    "revision": "f1335541fde03c9f60a1",
+    "url": "/julista_vk/static/js/main.65676a40.chunk.js"
   },
   {
     "revision": "386a623ce6c8f17e5991",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/julista_vk/precache-manifest.c8262a915f628713598e6ffab92d5f5e.js"
+  "/julista_vk/precache-manifest.6793f70660ed2bee865ea248b08869cc.js"
 );
 
 self.addEventListener('message', (event) => {
