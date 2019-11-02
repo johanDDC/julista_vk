ebc7e332887fd246da5999462254855fae6b0821
ivan <ivan.peshekhonov@gmail.com>
2019-11-02 19:12:43 +0300

    Fix loader

--- src/custom_components/support/customSpinner.js
+++ src/custom_components/support/customSpinner.js
@@ -3,31 +3,48 @@ import PropTypes from "prop-types";
 import "./customSpinner.css"
 import Icon44Spinner from '@vkontakte/icons/dist/44/spinner';
 import HalloweenPumpkin from "../eventual/halloween/HalloweenPumpkin"
+import {connect} from "react-redux";
 
-// const CustomSpinner = props => (
-//     <div className="customSpinnerContainer"
-//          style={{backgroundColor: (props.isInverse ? "var(--the-color)" : "var(--background)")}}>
-//         <div className="customSpinnerRotator"
-//              style={{color: (props.isInverse ? "var(--background)" : "var(--the-color)")}}>
-//             <Icon44Spinner/>
-//         </div>
-//     </div>
-// );
+class CustomSpinner extends React.Component {
+    constructor() {
+        super()
+    }
 
-const CustomSpinner = props => (
-    <div className="customSpinnerContainer"
-         style={{backgroundColor: (props.isInverse ? "var(--the-color)" : "var(--background)")}}>
-        <HalloweenPumpkin
-            scale={0.4}
-            shadow={false}
-            lighting={true}
-            floating={false}
-        />
-    </div>
-);
+    render() {
+        let inside;
+        if (this.props.theme === "dark") {
+            inside =
+                <HalloweenPumpkin
+                    scale={0.4}
+                    shadow={false}
+                    lighting={true}
+                    floating={false}
+                />
+        } else {
+            inside =
+                <div className="customSpinnerRotator"
+                     style={{color: (this.props.isInverse ? "var(--background)" : "var(--the-color)")}}>
+                    <Icon44Spinner/>
+                </div>
+        }
+        return (
+            <div className="customSpinnerContainer"
+                 style={{backgroundColor: (this.props.isInverse ? "var(--the-color)" : "var(--background)")}}>
+                {inside}
+            </div>
+        )
+    }
+
+}
 
 CustomSpinner.propTypes = {
     isInverse: PropTypes.bool.isRequired
 };
 
-export default CustomSpinner;
+const mapStateToProps = store => {
+    return {
+        theme: store.theme,
+    }
+};
+
+export default connect(mapStateToProps)(CustomSpinner);
