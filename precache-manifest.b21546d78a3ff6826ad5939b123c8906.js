b8df5efb775e3c0b8bbd0ff67220307fcbde4514
ivan <ivan.peshekhonov@gmail.com>
2019-11-03 12:18:31 +0300

    Updates

---
+++ precache-manifest.b21546d78a3ff6826ad5939b123c8906.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85734729ed6d22c8a40253111ef08d6f",
+    "url": "/julista_vk/index.html"
+  },
+  {
+    "revision": "fc08a8e28db936020d86",
+    "url": "/julista_vk/static/css/2.a83908c5.chunk.css"
+  },
+  {
+    "revision": "17e0b28761282802c0ca",
+    "url": "/julista_vk/static/css/main.9a2f3efb.chunk.css"
+  },
+  {
+    "revision": "fc08a8e28db936020d86",
+    "url": "/julista_vk/static/js/2.0a541b00.chunk.js"
+  },
+  {
+    "revision": "17e0b28761282802c0ca",
+    "url": "/julista_vk/static/js/main.cb2b1e99.chunk.js"
+  },
+  {
+    "revision": "386a623ce6c8f17e5991",
+    "url": "/julista_vk/static/js/runtime~main.bdba013e.js"
+  }
+]);
